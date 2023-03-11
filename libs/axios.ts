ec4e53ad5cef99c5c2ed00fa48e8c20aa069ae2d
hirotofuu <yamamotohirotofuu@gmail.com>
2023-03-11 13:07:36 +0900

    /

--- libs/axios.ts
+++ libs/axios.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
  
 const axios = Axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://localhost:8000',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
