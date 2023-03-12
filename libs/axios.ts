e18c97bbb68e6e4dc070b516e7994e4bd1f02a6a
hirotofuu <yamamotohirotofuu@gmail.com>
2023-03-12 11:00:48 +0900

    kaitei

--- libs/axios.ts
+++ libs/axios.ts
@@ -4,7 +4,7 @@ const axios = Axios.create({
     baseURL: 'https://newsbyteapi.info',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'content-type': 'application/json'
+        'Content-Type': 'application/json'
     },
     withCredentials: true,
 })
